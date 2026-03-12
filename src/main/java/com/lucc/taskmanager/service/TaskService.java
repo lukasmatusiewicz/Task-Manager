1ac7903b54c3fef6f0e4aece4c58e722f8b5e4dd
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-03-12 14:28:22 +0100

    Create TaskService.java

---
+++ src/main/java/com/lucc/taskmanager/service/TaskService.java
@@ -0,0 +1,40 @@
+package com.lucc.taskmanager.service;
+
+import com.lucc.taskmanager.model.Task;
+import com.lucc.taskmanager.model.User;
+import com.lucc.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService
+{
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository)
+    {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getTasksByUser(User user)
+    {
+        return taskRepository.findByUser(user);
+    }
+
+    public Task addTask(Task task, User user)
+    {
+        task.setUser(user);
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(int taskId, User user)
+    {
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new IllegalArgumentException("No such task with id: " + taskId));
+
+        if(!task.getUser().getUsername().equals(user.getUsername()))
+            throw new IllegalArgumentException("You can only delete your own tasks.");
+
+        taskRepository.delete(task);
+    }
+}
