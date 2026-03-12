58f951015297b0e8407e0e0f11c244a3c2a3d554
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-03-12 12:03:11 +0100

    Update UserController.java

--- src/main/java/com/lucc/taskmanager/controller/UserController.java
+++ src/main/java/com/lucc/taskmanager/controller/UserController.java
@@ -2,11 +2,7 @@ package com.lucc.taskmanager.controller;
 
 import com.lucc.taskmanager.model.User;
 import com.lucc.taskmanager.service.UserService;
-import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
