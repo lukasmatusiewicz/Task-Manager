098189f2e282c282e07603f12ac0cdcb8ba7fb41
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-03-12 14:28:17 +0100

    Create TaskController.java

---
+++ src/main/java/com/lucc/taskmanager/controller/TaskController.java
@@ -0,0 +1,39 @@
+package com.lucc.taskmanager.controller;
+
+import com.lucc.taskmanager.model.Task;
+import com.lucc.taskmanager.model.User;
+import com.lucc.taskmanager.service.TaskService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController
+{
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService)
+    {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getTaskByUser(@AuthenticationPrincipal User user)
+    {
+        return taskService.getTasksByUser(user);
+    }
+
+    @PostMapping
+    public Task addTask(@RequestBody Task task, @AuthenticationPrincipal User user)
+    {
+        return taskService.addTask(task, user);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(@PathVariable int taskId, @AuthenticationPrincipal User user)
+    {
+        taskService.deleteTask(taskId, user);
+    }
+}
