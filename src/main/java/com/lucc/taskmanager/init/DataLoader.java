2d61f32004d56a8f778e18ee5f4dc4290b2a75ce
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-03-12 14:02:22 +0100

    Update DataLoader.java

--- src/main/java/com/lucc/taskmanager/init/DataLoader.java
+++ src/main/java/com/lucc/taskmanager/init/DataLoader.java
@@ -3,7 +3,6 @@ package com.lucc.taskmanager.init;
 import com.lucc.taskmanager.model.Role;
 import com.lucc.taskmanager.model.User;
 import com.lucc.taskmanager.repository.UserRepository;
-import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,9 +27,11 @@ public class DataLoader implements CommandLineRunner
     public void run(String ... args) throws Exception
     {
         if (userRepository.count() == 0) {
-        User admin = new User("admin", passwordEncoder.encode("admin123"), "admin@gmail.com", Role.ADMIN);
-        User user = new User("user", passwordEncoder.encode("user123"), "user@gmail.com", Role.USER);
-        userRepository.saveAll(List.of(admin, user));
+            User admin = new User("admin", passwordEncoder.encode("admin123"), "admin@gmail.com", Role.ADMIN);
+            User user = new User("user", passwordEncoder.encode("user123"), "user@gmail.com", Role.USER);
+
+            userRepository.saveAll(List.of(admin, user));
+            System.out.println("Users loaded");
         }
     }
 }
