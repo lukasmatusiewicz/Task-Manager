38f4a14e9d6c0225e30ff4fcbc03622ce71d66f0
lukasmatusiewicz <matusiewiczlukas@gmail.com>
2026-04-08 15:51:06 +0200

    api summary and descriptions

--- src/main/java/com/lucc/taskmanager/controller/UserController.java
+++ src/main/java/com/lucc/taskmanager/controller/UserController.java
@@ -3,6 +3,8 @@ package com.lucc.taskmanager.controller;
 import com.lucc.taskmanager.model.User;
 import com.lucc.taskmanager.service.UserService;
 import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users", description = "Operations related to users management")
 public class UserController
 {
     private final UserService userService;
@@ -21,12 +24,14 @@ public class UserController
     }
 
     @GetMapping
+    @Operation(summary = "Get all users (Admin only)")
     public List<User> getUsers()
     {
         return userService.getUsers();
     }
 
     @PostMapping
+    @Operation(summary = "Add a new user (Admin only)")
     public User addUser(@Valid @RequestBody User user)
     {
         return userService.addUser(user);
